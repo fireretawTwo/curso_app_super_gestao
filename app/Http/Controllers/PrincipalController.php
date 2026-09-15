a0d8eeb77f5cf7efb4089ad0b4d7e2baef8d78e6
fireretaw <danilofernando1998@gmail.com>
2026-09-15 14:20:45 -0300

    First commit

---
+++ app/Http/Controllers/PrincipalController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PrincipalController extends Controller
+{
+    public function Principal() {
+        echo "Olá, Seja bem vindo!";
+    }
+}
